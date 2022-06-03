f7c5e1f18d625370ab2c472f1c5bc7c665cc09fd
Rony dos Santos Teles <rsantosr93@gmail.com>
2022-06-03 01:36:02 -0300

    Email agora é unique em Centro_doacao
    Como o valor para login é o email, ele deve ser único para cada linha dentro da tabela Centro_doacao.

--- sql/doacao_sql.sql
+++ sql/doacao_sql.sql
@@ -42,7 +42,7 @@ cnpj, login, senha (protegido por hash), nome, cep (ou endereço completo?)
 */
 CREATE TABLE Centro_doacao (
     cnpj CHAR(14),
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL,
     nome VARCHAR(255) NOT NULL,
     cep CHAR(8) NOT NULL,
